875d5d4abc1af1e5e22f5951a08abf39d071d07a
Travis Beck <tjbenator@gmail.com>
2014-07-17 16:12:42 +0000

    Added Config Sample.

---
+++ config.sample.php
@@ -0,0 +1,9 @@
+<?php
+define('DBDRIVER', 'mysql');
+define('DBNAME', 'status');
+define('DBUSERNAME', 'status');
+define('DBPASSWORD', 'YOUR_PASSWORD_HERE');
+define('DBHOSTNAME', 'localhost');
+
+define('accessorigin', 'YOUR_STATUS_DOMAIN_HERE');
+define('debug_mode', true);
